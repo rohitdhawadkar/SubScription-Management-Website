1dfbb78539742e124516a1447cf303161f5720fd
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-07-28 13:46:59 +0530

    added register route

---
+++ BackEnd/Register.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { z } = require("zod");
+const { Validate, route } = require("./MiddleWare");
+const pool = require("./DataBaseconfig");
+
+const router = express.Router();
+
+const RegisterSchema = z.object({
+  username: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+router.post("/register", Validate(RegisterSchema), async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const usernameResult = await pool.query("SELECT * From users WHERE username=$1", [username]);
+
+    if (usernameResult.rows.length > 0) {
+      return res.status(400).json({ "username already exist"});
+    }
+
+    const emailResult = await pool.query("SELECT * From users WHERE email=$1", [email]);
+
+    if (emailResult.rows.length > 0) {
+      return res.status(400).json({ "email already exist"});
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    await pool.query("INSERT INTO users (username,email,password) VALUES ($1,$2,$3)", [username, email, passwordHash]);
+    res.status(400).json({ "registration succesfull"})
+
+
+
+  } catch (err) {
+    res.json("error occured during registration");
+  }
+
+
+});

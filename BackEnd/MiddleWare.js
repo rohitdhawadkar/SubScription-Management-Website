ce5c63a0c03903d284a380fcaae3e72a75b30469
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-07-28 13:08:15 +0530

    Middleware

---
+++ BackEnd/MiddleWare.js
@@ -0,0 +1,22 @@
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const express = require("express");
+
+const router = express.Router();
+
+const Validate = (schema) => {
+  return (req, res, next) => {
+    if (!req.body) {
+      return res.status(400).json({ "request body is required"});
+    }
+
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (err) {
+      return res.status(400).json({ "invalid request body", errors: err.errors })
+    }
+  };
+};
+module.exports = router;
